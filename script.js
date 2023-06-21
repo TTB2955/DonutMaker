8cb4c99f747715b06da4aaa9058121a0fa503405
TTB2955 <tequan,browning81@gmail.com>
2023-06-21 17:10:40 -0500

    Final commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const dev = document.querySelector(".dev")
 const resetBtn = document.querySelector(".reset")
 
 const donutCount = document.querySelector(".donut-count")
-const donutBtn = document.querySelector(".get-donut")
+const donutBtn = document.querySelector(".make-donut")
 const donutValue = document.querySelector(".donut-value")
 
 const acValue = document.querySelector(".ac-count")
@@ -58,11 +58,13 @@ function autoClick() {
 updateDonutClicked(donutCount, donutMaker)
 donutMaker.makeAutoClickerWork()
 enableAutoClickerBtn()
+// enableMultiplierBtn()
 }
 
 const makeDonutBtn = (donutBtn, donutCount, donutMaker) => {
 donutBtn.addEventListener("click", () => {
     donutMaker.donutClicked()
+    // updateDonutCount(donutCount, donutMaker) the function is wrong
     updateDonutClicked(donutCount, donutMaker);
 })
 }
